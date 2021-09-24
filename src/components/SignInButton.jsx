1547aa0de1580c83678af0ed6f1a98d74cd420c6
Henry Mbugua <82150047+henrymbuguakiarie@users.noreply.github.com>
2021-09-24 15:04:54 +0300

    Update SignInButton.jsx
    @tnorling Proposing changes for named out-of-repo code ingestion into the docs.
    
    Cc: @mmacy

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -1,12 +1,15 @@
+// <ms_docref_import_libraries>
 import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../authConfig";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/esm/Dropdown";
+// </ms_docref_import_libraries>
 
 /**
  * Renders a drop down button with child buttons for logging in with a popup or redirect
  */
+// <ms_docref_constant_sign_in_button>
 export const SignInButton = () => {
     const { instance } = useMsal();
 
@@ -15,16 +18,25 @@ export const SignInButton = () => {
             instance.loginPopup(loginRequest).catch(e => {
                 console.log(e);
             });
+// </ms_docref_constant_sign_in_button>
+// <ms_docref_login_redirect>
         } else if (loginType === "redirect") {
             instance.loginRedirect(loginRequest).catch(e => {
                 console.log(e);
             });
+// </ms_docref_login_redirect>
+// <ms_docref_constant_sign_in_popup>
         }
     }
     return (
         <DropdownButton variant="secondary" className="ml-auto" drop="left" title="Sign In">
             <Dropdown.Item as="button" onClick={() => handleLogin("popup")}>Sign in using Popup</Dropdown.Item>
+// </ms_docref_constant_sign_in_popup>
+// <ms_docref_redirect_button>
             <Dropdown.Item as="button" onClick={() => handleLogin("redirect")}>Sign in using Redirect</Dropdown.Item>
+// </ms_docref_redirect_button>
+// <ms_docref_end_dropdown_button>
         </DropdownButton>
     )
 }
+// </ms_docref_end_dropdown_button>
