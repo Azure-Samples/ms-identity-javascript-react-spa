7803226fc57cc8186762db8a381f7740bab88fff
salman90 <v-ssalem@microsoft.com>
2022-10-03 16:58:09 -0700

    removed set active account

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './styles/App.css';
 import { PageLayout } from './components/PageLayout';
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
-import { InteractionStatus, InteractionRequiredAuthError } from '@azure/msal-browser';
 import Button from 'react-bootstrap/Button';
 import { loginRequest } from './authConfig';
 import { callMsGraph } from './graph';
@@ -13,37 +12,24 @@ import { ProfileData } from './components/ProfileData';
  * Renders information about the signed-in user or a button to retrieve data about the user
  */
 const ProfileContent = () => {
-    const { instance, inProgress  } = useMsal();
+    const { instance, accounts } = useMsal();
     const [graphData, setGraphData] = useState(null);
 
-    const RequestProfileData = () => {
-        
-        /**
-         * Silently acquires an access token which is then attached to a request for MS Graph data.  If the silent call failed we will use the interactive method.
-         */
-
-        if (inProgress === InteractionStatus.None) {
-            const request = {
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        instance
+            .acquireTokenSilent({
                 ...loginRequest,
-                account: instance.getActiveAccount(),
-            };
-
-            instance
-                .acquireTokenSilent(request)
-                .then((response) => {
-                    callMsGraph(response.accessToken).then((response) => setGraphData(response));
-                })
-                .catch((e) => {
-                    if (e instanceof InteractionRequiredAuthError) {
-                        instance.acquireTokenRedirect(request);
-                    }
-                });
-        }
-    };
+                account: accounts[0],
+            })
+            .then((response) => {
+                callMsGraph(response.accessToken).then((response) => setGraphData(response));
+            });
+    }
 
     return (
         <>
-            <h5 className="card-title">Welcome {instance.getActiveAccount()?.name}</h5>
+            <h5 className="card-title">Welcome {accounts[0].name}</h5>
             {graphData ? (
                 <ProfileData graphData={graphData} />
             ) : (
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
 import App from './App';
-import { PublicClientApplication, EventType } from '@azure/msal-browser';
+import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
 import { msalConfig } from './authConfig';
 
@@ -14,28 +14,6 @@ import { msalConfig } from './authConfig';
 const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Account selection logic is app dependent. Adjust as needed for different use cases.
-if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
-}
-
-/**
- * To set an active account after the user signs in, register an event and listen to LOGIN_SUCCESS & LOGOUT_SUCCES. For more,
- * visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/events.md
- */
-msalInstance.addEventCallback((event) => {
-    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
-        const account = event.payload.account;
-        msalInstance.setActiveAccount(account);
-    }
-
-    if (event.eventType === EventType.LOGOUT_SUCCESS) {
-         if (msalInstance.getAllAccounts().length > 0) {
-             msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
-         }
-    }
-});
 
 /**
  * We recommend wrapping most or all of your components in the MsalProvider component. It's best to render the MsalProvider as close to the root as possible.
